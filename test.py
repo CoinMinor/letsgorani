60cc41c9f4ff1e10e458eacdd95f497891fad493
timdind <128463556+timdind@users.noreply.github.com>
2025-07-01 13:38:23 +0900

    test
    test

---
+++ test.py
@@ -0,0 +1,10 @@
+# div = lambda x, y: x / y
+# print(div(10, 5))
+
+# mod = lambda x, y: x % y
+# print(mod(10, 3))
+
+# f_temp = [0, 10, 20, 30, 40, 50]
+
+# c_temp = map(lambda T: (T - 32) * 5 / 9, f_temp)
+# print(list(c_temp))
